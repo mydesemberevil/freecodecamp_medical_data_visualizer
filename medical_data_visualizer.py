a2825a532f5a2105fe470f9bd6994f0e843eb72a
mydesemberevil <72928226+mydesemberevil@users.noreply.github.com>
2024-09-11 18:46:30 +0700

    Created using Colab

---
+++ medical_data_visualizer.py
@@ -0,0 +1,45 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPODHOZ+mW5LWfs/1YY5mnW",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/mydesemberevil/freecodecamp_medical_data_visualizer/blob/main/medical_data_visualizer.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "id": "kJ7M-iCj4o6I"
+      },
+      "outputs": [],
+      "source": [
+        "import numpy as np\n",
+        "import pandas as pd\n",
+        "\n",
+        "\n",
+        "data = None"
+      ]
+    }
+  ]
+}
